951321f0a67b8b359fc7ef414c7b86760cf1204c
Juan Francisco Miranda Aguilar <jfmiranda75@hotmail.com>
2025-08-26 13:33:56 +0200

    Removed some tests

--- src/app/shared/directives/priority-color.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PriorityColor } from './priority-color';
-
-describe('PriorityColor', () => {
-  it('should create an instance', () => {
-    const directive = new PriorityColor();
-    expect(directive).toBeTruthy();
-  });
-});
